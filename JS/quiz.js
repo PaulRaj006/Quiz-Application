70fd42c7599b72454879fc72859d7537f7713454
PaulRaj006 <paulraj3062006@gmail.com>
2026-06-28 14:09:46 +0530

    update the updateResult( for score updation) function on the Popup box

--- JS/quiz.js
+++ JS/quiz.js
@@ -2,7 +2,10 @@ let nextBtn = document.getElementById('nextBtn');
 let ques = document.querySelector('.ques');
 let ans = document.querySelectorAll('.let-ans');
 let fill = document.querySelector('.fill');
-let btn = document.querySelectorAll('.btn')
+let btn = document.querySelectorAll('.btn');
+
+let name = "Paul raj";
+let attempted = false;    //check user skip or fail in quiz
 
 let currentQuestion = 0;
 
@@ -63,6 +66,7 @@ function nextQuestion(){
         ques10.textContent = `Question ${count} of 10`;
     }
     else{
+        updateResult()
         let overlay = document.querySelector('.overlay-hidden');
         let popup = document.querySelector('.popup-box');
         overlay.style.display = "flex";
@@ -81,10 +85,12 @@ function nextQuestion(){
 }
 
 // compar the option with answer :
-let score =0;
+let score = 0;
 
 btn.forEach((item)=>{
     item.onclick = function(){
+        attempted = true;
+        clearInterval(timer);     //stop the current timer
         let qiuzAnswer = gkQuestions[currentQuestion];
         let last = item.querySelector('.last');
         let selectedAnswer = item.querySelector('.let-ans');
@@ -96,6 +102,7 @@ btn.forEach((item)=>{
             selectedOption.style.backgroundColor = "#4ff75a"
             selectedOption.style.color = "white"
             score++;
+            console.log(score);  
         }
         else{
             item.classList.add("wrong");  //create new "wrong" class;
@@ -120,3 +127,72 @@ btn.forEach((item)=>{
     }
 });
 
+        
+
+        // score updatation =========== popup ============= 
+        // work agala ennanu kandupudi...............
+        
+function updateResult(){
+        let scorePer = document.getElementById('scorePersentage')
+        let percentage = score ;
+        let degree = percentage * 36;
+        scorePer.innerHTML = `${score*10} %`
+        console.log(degree);
+        let correct = document.getElementById('Correct-no');
+        let scoreout = document.getElementById('scor-outof');
+        let wrong = document.getElementById('wrong-no');
+        let circle = document.querySelector('.circle');
+
+        let instruct = document.getElementById('instruction');
+
+        let pass_fail = document.querySelector('.pass-fail');
+        let icone = pass_fail.querySelector('#icone');
+        let status = pass_fail.querySelector('#status');
+
+        correct.textContent = score;
+        scoreout.textContent = score;
+        wrong.textContent = 10 - score;
+
+        circle.style.background = `conic-gradient(#4F6EF7 0deg ${degree}deg,#ddd ${degree}deg 360deg)`;
+
+        // ✗ ✓
+        if(score>=6){
+            icone.textContent = "✓"
+            icone.style.color = "green";
+            status.textContent = "Passed"
+            status.style.color = "green";
+            pass_fail.style.border = "2px solid rgb(90, 242, 90)";
+            pass_fail.style.backgroundColor = "#cbfac1"
+            instruct.innerHTML = `🎉 Well Done, <span>${name}</span>!`;
+        }
+        else if (score == 5) {
+            // Average
+            icone.textContent = "!";
+            icone.style.color = "#f59e0b";
+            status.textContent = "Average";
+            status.style.color = "#f59e0b";
+            pass_fail.style.border = "2px solid #fcd34d";
+            pass_fail.style.backgroundColor = "#fef3c7";
+            instruct.innerHTML = `👍 Good Try, <span>${name}</span>!`;
+        }
+        else if(score == 0){
+            icone.textContent = "✗"
+            icone.style.color = "red";
+            status.textContent = "Failed"
+            status.style.color = "red";
+            pass_fail.style.border = "2px solid rgb(242, 90, 90)";
+            pass_fail.style.backgroundColor = "#facec1"
+            if(attempted){
+                instruct.innerHTML = `😔 Better Luck Next Time, <span>${name}</span>!`;
+            }
+}
+        else{
+            icone.textContent = "✗"
+            icone.style.color = "red";
+            status.textContent = "Failed"
+            status.style.color = "red";
+            pass_fail.style.border = "2px solid rgb(242, 90, 90)";
+            pass_fail.style.backgroundColor = "#facec1"
+            instruct.innerHTML = `💪 Keep Practising, <span>${name}</span>!`;
+        }
+}
