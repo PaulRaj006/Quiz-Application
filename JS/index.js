8045c331dffc8c34c88e0a5a6e091550455c6aa5
PaulRaj006 <paulraj3062006@gmail.com>
2026-06-29 21:17:32 +0530

    update quiz.js (add active-category&activeBtn class when user click box&btn)

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,28 @@
 let start = document.getElementById('stBtn');
 
+// Category
 let category = "";
 let boxes = document.querySelectorAll('.box');
 boxes.forEach((item)=>{
     item.onclick = function(){
+        boxes.forEach((box)=>{
+            box.classList.remove("active-category");
+        })
+        item.classList.add("active-category");
         category = item.dataset.category;
         console.log(category); // for checking porpose 
     }
 });
 
+// Difficulty
 let diffLevel ="";
 let diffBtn = document.querySelectorAll('.btn');
 diffBtn.forEach((item)=>{
     item.onclick = function(){
+        diffBtn.forEach((btn)=>{
+            btn.classList.remove("activeBtn");
+        })
+        item.classList.add("activeBtn");
         diffLevel = item.dataset.difflevel;
         console.log(diffLevel); // for checking porpose
     };
@@ -23,6 +33,20 @@ start.onclick = function(){
     localStorage.setItem("userName",name);
     localStorage.setItem("category",category);
     localStorage.setItem("diffLevel", diffLevel);
+
+    //check user validation or not
+    if(name.trim()==""){
+        alert("please enter your name");
+        return;
+    }
+    if(category == ""){
+        alert("PLease select a category")
+        return;
+    }
+    if(diffLevel == ""){
+        alert("please select difficulty");
+        return;
+    }
 
     //itha last add pannanum
     window.location.href = "qiuz.html";
