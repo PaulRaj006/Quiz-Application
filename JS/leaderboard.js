ebcbe337b756164c198e58d3095c72171b0a485d
PaulRaj006 <paulraj3062006@gmail.com>
2026-07-10 23:12:04 +0530

    update leaderboard.js()

--- JS/leaderboard.js
+++ JS/leaderboard.js
@@ -1,55 +1,130 @@
-let sortBtn = document.getElementById('sortBtn');
+let sortBtn = document.getElementById("sortBtn");
 let data = JSON.parse(localStorage.getItem("leaderboard")) || [];
 let tbody = document.getElementById("leaderboardBody");
 
-let assending = true;
+let ascending = false;
+
 function sortList(){
-    if(assending){
-        data.sort((a,b)=>a.score-b.score);
+    console.log("Button Clicked")
+    if(ascending){
+        // Highest
+        data.sort((a,b)=>Number(b.score)-Number(a.score));
+        
+        sortBtn.innerHTML =
+        `<i class="fa-solid fa-arrow-up-wide-short"></i> Sort: Highest Score`;
+        
+    }else{
+        // Lowest
+        data.sort((a,b)=>Number(a.score)-Number(b.score));
 
-        sortBtn.innerHTML=`<i class="fa-solid fa-arrow-down-wide-short"></i> Sort: Lowest Score`
-        assending = false;
+        sortBtn.innerHTML =
+        `<i class="fa-solid fa-arrow-down-wide-short"></i> Sort: Lowest Score`;
+        
     }
-    else{
-        data.sort((a,b)=>b.score-a.score);
-
-        sortBtn.innerHTML=`<i class="fa-solid fa-arrow-up-wide-short"></i> Sort: Highest Score`
-        assending = true;
-    }
-    displayLeaderboard();
+    ascending = !ascending;
+    displayTopPlayers();
+    displayTable();
 }
 
-data.sort((a,b)=>b.score-a.score);
-displayLeaderboard();
+// Initial Highest Sort
+data.sort((a,b)=>Number(b.score)-Number(a.score));
+const topThree = data.slice(0,3);
+const remaining = data.slice(3);
+displayTopPlayers();
+displayTable();
 
-// displayLeaderboard function
-function displayLeaderboard(){
+function displayTopPlayers(){
+
+    let first = data[0];
+    let second = data[1];
+    let third = data[2];
+
+    if(first){
+        document.querySelector(".top1 .name").innerHTML = first.name;
+        document.querySelector(".top1 .score").innerHTML = first.score+"%";
+        document.querySelector(".top1 .category").innerHTML =
+        `${first.category} · ${first.difficulty}`;
+
+        document.querySelector(".top1 .profileTop").innerHTML =
+        first.name.charAt(0).toUpperCase();
+        document.querySelector('.position1').innerHTML = "#1";
+    }
+
+    if(second){
+        document.querySelector(".top2 .name").innerHTML = second.name;
+        document.querySelector(".top2 .score").innerHTML = second.score+"%";
+        document.querySelector(".top2 .category").innerHTML =
+        `${second.category} · ${second.difficulty}`;
+
+        document.querySelector(".top2 .profileTop").innerHTML =
+        second.name.charAt(0).toUpperCase();
+        document.querySelector('.position2').innerHTML = "#2";
+    }
+
+    if(third){
+        document.querySelector(".top3 .name").innerHTML = third.name;
+        document.querySelector(".top3 .score").innerHTML = third.score+"%";
+        document.querySelector(".top3 .category").innerHTML =
+        `${third.category} · ${third.difficulty}`;
+
+        document.querySelector(".top3 .profileTop").innerHTML =
+        third.name.charAt(0).toUpperCase();
+        document.querySelector('.position3').innerHTML = "#3";
+    }
+
+}
+
+function displayTable(){
+    console.table(data);
+
     tbody.innerHTML = "";
+    const remaining = data.slice(3);
+    remaining.forEach((item,index)=>{
 
-    data.forEach((item,index)=>{
-        let color;
-        if(item.score >= 80){
-            color="green";
-        }
-        else if(item.score >= 50){
-            color="orange";
-        }
-        else{
-            color="red";
-        }
+    let color = "red";
 
-        tbody.innerHTML += `
-        <tr>
-            <td>#${index+1}</td>
-            <td>${item.name}</td>
-            <td>${item.category}</td>
-            <td>${item.difficulty}</td>
-            <td style="color:${color};font-weight:bold">
-                ${item.score}%
-            </td>
-            <td>${item.date}</td>
-            <td>${item.time}</td>
-        </tr>
-        `;
-    });
+    if(item.score >= 80){
+        color = "green";
+    }else if(item.score >= 50){
+        color = "orange";
+    }
+    // First letter || 1st 2nd letter
+    let initial = item.name.trim().split(" ").map(word => word[0]).join("").substring(0,2).toUpperCase();
+    // Random profile coloiir
+    const profileColors = [
+        "#4F46E5",
+        "#F59E0B",
+        "#10B981",
+        "#EF4444",
+        "#8B5CF6",
+        "#06B6D4",
+        "#EC4899",
+        "#F97316"
+    ];
+
+    let profileColor = profileColors[index % profileColors.length];
+
+    tbody.innerHTML += `
+    <tr>
+        <td>#${index + 4}</td>
+        <td>
+            <div class="player-info">
+                <div class="profile"
+                     style="background:${profileColor}">
+                    ${initial}
+                </div>
+                <span>${item.name}</span>
+            </div>
+        </td>
+
+        <td>${item.category}</td>
+        <td>${item.difficulty}</td>
+
+        <td style="color:${color};font-weight:bold">
+            ${item.score}%
+        </td>
+        <td>${item.date}</td>
+        <td>${item.time}</td>
+    </tr>`;
+});
 }
