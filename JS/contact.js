caee4bf6f6c1ea3ba11281da1db3fdacc3a45628
PaulRaj006 <paulraj3062006@gmail.com>
2026-07-24 23:39:45 +0530

    create contact.js(for intract the contact.html)

---
+++ JS/contact.js
@@ -0,0 +1,44 @@
+// section-box page open
+const leaderboardBtn = document.querySelector(".leaderboard");
+leaderboardBtn.addEventListener("click", () => {
+    console.log("Leaderboard clicked");
+    window.location.href = "leaderboard.html";
+});
+const indexBtn = document.querySelector(".index");
+indexBtn.addEventListener("click", () => {
+    console.log("Home clicked");
+    window.location.href = "index.html";
+});
+const historyBtn = document.querySelector(".history");
+historyBtn.addEventListener("click", () => {
+    console.log("history clicked");
+    window.location.href = "history.html";
+});
+const aboutBtn = document.querySelector(".about");
+aboutBtn.addEventListener("click", () => {
+    console.log("about clicked");
+    window.location.href = "about.html";
+});
+const contactBtn = document.querySelector(".contact");
+contactBtn.addEventListener("click", () => {
+    console.log("contact clicked");
+    window.location.href = "contact.html";
+});
+
+// Star rating
+let selectedRating = 0;
+const stars = document.querySelectorAll("#starRating i");
+stars.forEach(star=>{
+    star.addEventListener("click", function(){
+        selectedRating = Number(this.dataset.v);
+        stars.forEach(s=>{
+            if(Number(s.dataset.v) <= selectedRating){
+                s.classList.remove("fa-regular");
+                s.classList.add("fa-solid");
+            } else {
+                s.classList.remove("fa-solid");
+                s.classList.add("fa-regular");
+            }
+        });
+    });
+});
