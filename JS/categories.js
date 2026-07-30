889a7cc1fa1e5f164779ffcf5cb5b5e0617ed934
PaulRaj006 <paulraj3062006@gmail.com>
2026-07-30 23:22:42 +0530

    create category.html,css,js file

---
+++ JS/categories.js
@@ -0,0 +1,36 @@
+// section-box page open
+const leaderboardBtn = document.querySelector(".leaderboard");
+leaderboardBtn.addEventListener("click", () => {
+    console.log("Leaderboard clicked");
+    window.location.href = "leaderboard.html";
+});
+const indexBtn = document.querySelector(".index");
+indexBtn.addEventListener("click", () => {
+    console.log("Home clicked");
+    window.location.href = "index.html";
+});
+const historyBtn = document.querySelector(".history");
+historyBtn.addEventListener("click", () => {
+    console.log("history clicked");
+    window.location.href = "history.html";
+});
+const aboutBtn = document.querySelector(".about");
+aboutBtn.addEventListener("click", () => {
+    console.log("about clicked");
+    window.location.href = "about.html";
+});
+const contactBtn = document.querySelector(".contact");
+contactBtn.addEventListener("click", () => {
+    console.log("contact clicked");
+    window.location.href = "contact.html";
+});
+const categoriesBtn = document.querySelector(".categories");
+categoriesBtn.addEventListener("click", () => {
+    console.log("contact clicked");
+    window.location.href = "categories.html";
+});
+const dashboardBtn = document.querySelector(".dashboard");
+dashboardBtn.addEventListener("click", () => {
+    console.log("dashboard clicked");
+    window.location.href = "dashboard.html";
+});
